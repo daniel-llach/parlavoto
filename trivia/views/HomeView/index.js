ed5b0e94035da4dbd7b768021ea62e9f11865090
daniel-llach <daniel.llach@gmail.com>
2019-09-05 11:49:29 -0400

    Add navigation and kickoff homeview

---
+++ trivia/views/HomeView/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class HomeView extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TRIVIA</Text>
+      </View>
+    )
+  }
+}
+
+export default HomeView
